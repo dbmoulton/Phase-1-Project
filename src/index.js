94a6eb6d3a8c0a1c2e410711132c51387f30aeaf
David Moulton <dbmoulton@gmail.com>
2023-04-25 20:42:27 -0400

    fixed get request

--- src/index.js
+++ src/index.js
@@ -1,7 +1,11 @@
+let divCollect = document.querySelector('#toy-collection')
+
+
 function getAmiibo() {
     return fetch('https://www.amiiboapi.com/api/amiibo')
       .then(res => res.json())
-  }
+}
+
 function renderAmiibo(amiibo) {
   let h2 = document.createElement('h2')
   h2.innerText = amiibo.name
@@ -11,7 +15,7 @@ function renderAmiibo(amiibo) {
   img.setAttribute('class', 'toy-avatar')
 
   let p = document.createElement('p')
-  p.innerText = `${toy.likes} likes`
+  p.innerText = `Game Series: ${amiibo.gameSeries} \n Amiibo Series: ${amiibo.amiiboSeries}`
 
   let btn = document.createElement('button')
   btn.setAttribute('class', 'like-btn')
@@ -24,15 +28,16 @@ function renderAmiibo(amiibo) {
 
   let divCard = document.createElement('div')
   divCard.setAttribute('class', 'card')
-  divCard.append(h2, img, p, btn)
+  divCard.append(h2, img, p)
   divCollect.append(divCard)
 }
 
-renderAmiibo(amiibo)
 
-// getAmiibo().then(amiibo => {
-//   amiibo.forEach(amiibo => {
-//     //function to render toys goes here or something
-//     renderAmiibo(amiibo)
-//   })
-// })
+getAmiibo().then(amiibos => {
+  console.log(amiibos)
+  amiibos.amiibo.forEach(data => {
+    renderAmiibo(data)
+  })
+})
+
+
